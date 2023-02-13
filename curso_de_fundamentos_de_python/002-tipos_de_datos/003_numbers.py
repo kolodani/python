5734859db39f5420abf4ce92a548eb16609d11e3
kolodani <danielvinzia@gmail.com>
2023-02-13 17:51:02 -0300

    python, clase 003, numbers

---
+++ curso_de_fundamentos_de_python/002-tipos_de_datos/003_numbers.py
@@ -0,0 +1,39 @@
+lives = 3
+print(type(lives))
+age = 53
+budget = 100
+
+temperatura = 36.51
+print(type(temperatura))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 4500000000000000000.1
+print(number)
+
+number_b = 0.0000000000000001
+print(number_b)
+
+enero = 159
+febrero = 154
+marzo = 149
+promedio = (enero + febrero + marzo) / 3
+print(promedio)
