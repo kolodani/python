fa675f08b317d600416e86884e55da2bf3b31df3
kolodani <danielvinzia@gmail.com>
2023-02-13 18:10:00 -0300

    python, clase 005, transformations

---
+++ curso_de_fundamentos_de_python/002-tipos_de_datos/005_transformation.py
@@ -0,0 +1,20 @@
+name = "Grover"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Grover" + " " + "Cleveland")
+print(10 + 20)
+print("Daniel" + "12")
+
+age = 12
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input('Escribe tu edad => ')
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 años sera {age}')
