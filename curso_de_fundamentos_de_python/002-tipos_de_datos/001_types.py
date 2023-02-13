15d498f4db347c74f6fa931d1d4c1b053b6e856f
kolodani <danielvinzia@gmail.com>
2023-02-13 17:23:11 -0300

    python, clase 001, types

---
+++ curso_de_fundamentos_de_python/002-tipos_de_datos/001_types.py
@@ -0,0 +1,20 @@
+# string
+my_name = "Juan"
+my_name = 'Daniel'
+print('my_name =>', my_name)
+print(type(my_name))
+
+# int
+my_age = 25
+print('my_age =>', my_age)
+print(type(my_age))
+
+# booolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('¿Cuál es tu edad? ')
+print('my_age =>', my_age)
+print(type(my_age))
