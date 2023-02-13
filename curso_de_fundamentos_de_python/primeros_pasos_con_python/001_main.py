38fe4b0eb582118fdeb5cf706f13b2f5616948dc
kolodani <danielvinzia@gmail.com>
2023-02-13 16:46:34 -0300

    python, clase 001

---
+++ curso_de_fundamentos_de_python/primeros_pasos_con_python/001_main.py
@@ -0,0 +1,24 @@
+print("Hola, esto es python")
+
+print("Hola soy Daniel y tengo 20 años")
+
+# Operaciones matemáticas
+
+#print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+
+"""
+Esto es un comentario
+de varias lineas
+"""
+
+'''
+Comentario
+de
+varias
+lineas
+'''
