e6dd77e44f69fe68d90e3f0c45200423b3528e4d
kolodani <danielvinzia@gmail.com>
2023-02-13 16:58:54 -0300

    python, clase 002, variables

---
+++ curso_de_fundamentos_de_python/primeros_pasos_con_python/002_vars.py
@@ -0,0 +1,16 @@
+print("Hola, vars")
+
+# esto es una variable
+my_name = "Daniel"
+print(my_name)
+
+# esto es una variable con un número
+my_age = 20
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio", my_name)
+
+# input
+my_name = input("¿Cual es tu nombre? ")
+print("Usando input", my_name)
