d2cf7376ff735c32343e62626c720db81f17f679
kolodani <danielvinzia@gmail.com>
2023-02-14 16:34:27 -0300

    python, clase 009, map dicts

---
+++ curso_de_python_avanzado/003-funciones/009-map_dicts.py
@@ -0,0 +1,24 @@
+items = [
+    {
+        'product': 'camisa',
+        'price': 100,
+    },
+    {
+        'product': 'pantalones',
+        'price': 300
+    },
+    {
+        'product': 'zapatos',
+        'price': 200
+    }
+]
+
+prices = list(map(lambda item: item['price'],items))
+print(prices)
+
+def add_taxes(item):
+    item['taxes'] = item['price'] * 0.19
+    return item
+
+new_items = list(map(add_taxes, items))
+print(new_items)
