30a954f11708f8fd5798549220cd37a7a7ff06cb
kolodani <danielvinzia@gmail.com>
2023-02-14 11:16:02 -0300

    python, proyecto terminado

--- curso_de_fundamentos_de_python/proyecto.py
+++ curso_de_fundamentos_de_python/proyecto.py
@@ -2,37 +2,64 @@ import random
 
 options = ('piedra', 'papel', 'tijera')
 
-user_option = input('piedra, papel o tijera? => ')
-user_option = user_option.lower()
+rounds = 1
+computer_wins = 0
+user_wins = 0
 
-if not user_option in options:
-    print('Esa opcion no es valida')
+while computer_wins < 2 and user_wins < 2:
 
-computer_option = random.choice(options)
+    print('*' * 10)
+    print('RONDA', rounds)
+    print('*' * 10)
 
-print('User option =>', user_option)
-print('Computer option =>', computer_option)
+    print('computadora =>', computer_wins)
+    print('usuario =>', user_wins)
 
-if user_option == computer_option:
-    print('Empate!')
-elif user_option == 'piedra':
-    if computer_option == 'tijera':
-        print('piedra gana a tijera')
-        print('Ganaste!')
-    else:
-        print('papel gana a piedra')
-        print('Perdiste!')
-elif user_option == 'papel':
-    if computer_option == 'piedra':
-        print('papel gana a piedra')
-        print('Ganaste!')
-    else:
-        print('tijera gana a papel')
-        print('Perdiste!')
-elif user_option == 'tijera':
-    if computer_option == 'papel':
-        print('tijera gana a papel')
-        print('Ganaste!')
-    else:
-        print('piedra gana a tijera')
-        print('Perdiste!')
+    user_option = input('piedra, papel o tijera? => ')
+    user_option = user_option.lower()
+
+    if not user_option in options:
+        print('Esa opcion no es valida')
+        continue
+
+    computer_option = random.choice(options)
+
+    print('User option =>', user_option)
+    print('Computer option =>', computer_option)
+
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('piedra gana a tijera')
+            print('Ganaste!')
+            user_wins += 1
+        else:
+            print('papel gana a piedra')
+            print('Perdiste!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('papel gana a piedra')
+            print('Ganaste!')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('Perdiste!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('tijera gana a papel')
+            print('Ganaste!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('Perdiste!')
+            computer_wins += 1
+
+    rounds += 1
+
+if computer_wins == 2:
+    print('Perdiste el juego!')
+else:
+    print('Ganaste el juego!')
