ab1488e43ca7e3518ea43ec0378d9a66de6c84a8
kolodani <danielvinzia@gmail.com>
2023-02-14 08:21:28 -0300

    python, proyecto 1.2

---
+++ curso_de_fundamentos_de_python/proyecto.py
@@ -0,0 +1,28 @@
+user_option = input('piedra, papel o tijera? => ')
+computer_option = 'papel'
+
+user_option = user_option.lower()
+
+if user_option == computer_option:
+    print('Empate!')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('piedra gana a tijera')
+        print('Ganaste!')
+    else:
+        print('papel gana a piedra')
+        print('Perdiste!')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('papel gana a piedra')
+        print('Ganaste!')
+    else:
+        print('tijera gana a papel')
+        print('Perdiste!')
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        print('tijera gana a papel')
+        print('Ganaste!')
+    else:
+        print('piedra gana a tijera')
+        print('Perdiste!')
