30f32d88b31f3e1af31c6da5f778163799548429
kolodani <danielvinzia@gmail.com>
2023-02-14 00:22:48 -0300

    python, clase 006, if

---
+++ curso_de_fundamentos_de_python/003-operadores_y_condicionales/006-if.py
@@ -0,0 +1,32 @@
+if True:
+    print('deberia ejecutarse')
+
+if False:
+    print('nunca se ejecuta')
+
+'''
+pet = input('¿Cual es tu mascota favorita? ')
+
+if pet == 'perro':
+    print('Genial tiene buen gusto')
+elif pet == 'gato':
+    print('Espero tengas suerte')
+elif pet == 'pez':
+    print('Eres lo maximo')
+else:
+    print('No tienes ninguna mascota interesante')
+
+stock = int(input('Digita el stock => '))
+
+if stock >= 100 and stock <= 1000:
+    print('El stock es correcto')
+else:
+    print('El stock es incorrecto')
+'''
+
+# ? Reto de la clase
+numero = int(input('ingresa un numero entero => '))
+if (numero % 2) == 0:
+    print('El numero es par')
+elif (numero % 2) == 1:
+    print('El numero es impar')
