689671cc882524d35c01220f30b8d55b5a051c4b
kolodani <danielvinzia@gmail.com>
2023-02-14 09:32:10 -0300

    python, clase 005, tuples

---
+++ curso_de_fundamentos_de_python/004-listas_y_diccionarios/005-tuples.py
@@ -0,0 +1,23 @@
+numbers = (1,2,3,5)
+strings = ('nico', 'zule', 'santi', 'nico')
+print(numbers)
+print('0 =>', numbers[0])
+print('-1 =>', numbers[-1])
+print(type(numbers))
+
+print(strings)
+print(type(strings))
+
+print(strings)
+print(strings.index('zule'))
+print(strings.count('nico'))
+
+my_list = list(strings)
+print(my_list)
+print(type(my_list))
+
+my_list[1] = 'juli'
+print(my_list)
+
+my_tuple = tuple(my_list)
+print(my_tuple)
